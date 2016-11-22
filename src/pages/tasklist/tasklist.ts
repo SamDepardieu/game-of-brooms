9797980ac3b9d6cc403d00aed29d5383a3f32407
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-11-22 15:04:30 +0100

    LLE - Add a tasklist

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Taskadd } from '../taskadd/taskadd';
 import { Taskdetail } from '../taskdetail/taskdetail';
 
@@ -10,12 +10,22 @@ import { Taskdetail } from '../taskdetail/taskdetail';
 })
 export class Tasklist {
 
-  constructor(public navCtrl: NavController) {
-    
+
+	public items; 
+
+  constructor(public navCtrl: NavController, navParams: NavParams) {
+    this.items = [{title: 'dede', data:'cocuouc'}, {title: 'eded', data:'sdfsqdf'}];
   }
   public goToTaskAdd(): void
   {
   	this.navCtrl.push(Taskadd); 
+  }
+  public clickOnItem(item): void
+  {
+  	this.navCtrl.push(Taskdetail, 
+  	{
+  		data: item
+  	});
   }
 
 }
