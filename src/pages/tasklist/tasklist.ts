eb5bac076d5ef5f856e6c25a7d4f23e5c5e585ed
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-11-22 15:52:07 +0100

    LLE - Prepare the list setup

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -13,13 +13,25 @@ export class Tasklist {
 
 	public items; 
 
-  constructor(public navCtrl: NavController, navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  	// Feed the items array
     this.items = [{title: 'dede', data:'cocuouc'}, {title: 'eded', data:'sdfsqdf'}];
   }
+
+
+  /**
+   * go to taskadd page
+   */
   public goToTaskAdd(): void
   {
   	this.navCtrl.push(Taskadd); 
   }
+
+  /**
+   * function when you click on item 
+   * @param {item} item item
+   */
   public clickOnItem(item): void
   {
   	this.navCtrl.push(Taskdetail, 
