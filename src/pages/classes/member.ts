428a0df620a156e2cf151656289dd37f4631a1be
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-11-25 10:41:37 +0100

    LLE - setup the classes

--- src/pages/classes/member.ts
+++ src/pages/classes/member.ts
@@ -1,4 +1,19 @@
+import { Group } from './group'
+
 export class Member
 {
-	
+	id: number;
+	group: Group; 
+	name: string;
+	points: number;
+	isAdmin: number;
+
+	constructor(id: number, group: Group, name: string, points: number, isAdmin: number)
+	{
+		this.id = id;
+		this.group = group;
+		this.name = name; 
+		this.points = points; 
+		this.isAdmin = isAdmin;
+	}
 }
