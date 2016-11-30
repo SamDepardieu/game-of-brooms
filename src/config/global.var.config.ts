f3475a9515f275309976c92ca0282d20c8f18f2d
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-11-30 10:24:57 +0100

    LLE - Changes

--- src/config/global.var.config.ts
+++ src/config/global.var.config.ts
@@ -1,4 +1,7 @@
 export class GlobalConfig
 {
-	public static get API_URL(): string { return "http://10.176.50.45/www/brooms/api.php"; }
+	public static get API_URL(): string { return "http://10.176.50.45/www/brooms/api.php "; }
 }
+
+// brooms.b2plus.com/api.php
+// http://10.176.50.45/www/brooms/api.php 
