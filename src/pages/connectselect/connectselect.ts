9759b5776ef2d20e674182365305b1802af6dbcd
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-12-02 14:28:23 +0100

    LLE - Comments

--- src/pages/connectselect/connectselect.ts
+++ src/pages/connectselect/connectselect.ts
@@ -13,6 +13,10 @@ import { PouchDBService } from '../../services/pouchdb.service';
 	selector: 'page-connectselect',
 	templateUrl: 'connectselect.html'
 })
+/**
+ * The ConnectselectPage class / component 
+ * @type {ConnectselectPage}
+ */
 export class ConnectselectPage 
 {
 	/**
@@ -35,10 +39,10 @@ export class ConnectselectPage
 
 	/**
 	 * The ConnectselectPage constructor 
-	 * @param {PouchDBService} private pouchdbService [description]
-	 * @param {UserService}    private userService    [description]
-	 * @param {NavController}  public  navCtrl        [description]
-	 * @param {NavParams}      public  navParams      [description]
+	 * @param {PouchDBService} private pouchdbService Service use to call pouchDB methods
+	 * @param {UserService}    private userService    Service use to manipulate user data 
+	 * @param {NavController}  public  navCtrl        Nav controller for routing 
+	 * @param {NavParams}      public  navParams      Nav params for data bindings in routing
 	 */
 	constructor(private pouchdbService: PouchDBService, private userService: UserService, public navCtrl: NavController, public navParams: NavParams) {}
 
@@ -47,8 +51,8 @@ export class ConnectselectPage
 	 */
 	ngOnInit()
 	{
-		this.pouchdbService.sync(); 
-		console.log()
+		// Launch the sync for the remote and local dbs
+		this.pouchdbService.sync();
 	}
 
 	/**
@@ -58,6 +62,7 @@ export class ConnectselectPage
 	{
 		let connect = this.userService.get(this.userMail).then((response) => 
 		{
+			console.log('Connection', response);
 			this.navCtrl.push(HomePage, 
 			{
 				userParams: this.userMail
@@ -87,7 +92,7 @@ export class ConnectselectPage
 
 		this.userService.add(newUser).then((response) => 
 		{
-			console.log(response);
+			console.log('User added', response);
 		}).catch((error) =>
 		{
 			console.error(error); 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,39 @@
+// Angular Import 
 import { Injectable } from '@angular/core';
-import { GlobalConfig } from '../config/global.var.config'; 
+
+// Import Config 
+import { GlobalConfig } from '../config/global.var.config';
+
+// Services Import  
 import { PouchDBService } from './pouchdb.service'; 
 
 @Injectable()
+/**
+ * The UserService class / service 
+ * @type {[type]}
+ */
 export class UserService
 {
+	/**
+	 * The local db var 
+	 * @type {PouchDB}
+	 */
 	private _db;
 
+	/**
+	 * The UserService constructor 
+	 * @param {PouchDBService} private pouchdbService Service use to call pouchDB methods
+	 */
 	constructor(private pouchdbService: PouchDBService)
 	{
 		this._db = this.pouchdbService.db;
 	}
 
+	/**
+	 * Get a user function 
+	 * @param  {string} id The mail of user 
+	 * @return {any}       Object with the docs 
+	 */
 	public get(id: string): any
 	{
 		return this._db.get(id).then((response) => 
@@ -23,9 +45,14 @@ export class UserService
 		});
 	}
 
+	/**
+	 * Add a user function 
+	 * @param  {Object} obj The complete doc to send to db
+	 * @return {any}        Object with the docs 
+	 */
 	public add(obj: Object): any
 	{
-		this._db.put(obj).then((response) => 
+		return this._db.put(obj).then((response) => 
 		{
 			return response;
 		}).catch((error) =>
@@ -34,7 +61,19 @@ export class UserService
 		});
 	}
 
-	public delete(id: string): any
+	/**
+	 * Remove a user function 
+	 * @param  {Object} doc The complete doc to remove 
+	 * @return {any}        Object with the docs 
+	 */
+	public remove(doc: Object): any 
 	{
+		return this._db.remove(doc).then((response) => 
+		{
+			return response; 
+		}).catch((error) => 
+		{
+			throw error; 
+		}); 
 	}
 }
