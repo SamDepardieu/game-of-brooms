044f57087b0e85d0b0c6710e0fff95fea5131960
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-12-02 15:46:53 +0100

    LLE - Add some methods for tasks

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,25 +1,85 @@
+// Angular Import 
 import { Injectable } from '@angular/core';
+
+// Import Config 
 import { GlobalConfig } from '../config/global.var.config'; 
+
+// Services import 
 import { PouchDBService } from './pouchdb.service'; 
 
 @Injectable()
+/**
+ * The TaskService class / service 
+ * @type {TaskService}
+ */
 export class TaskService
 {
+	/**
+	 * The local db var 
+	 * @type {PouchDB}
+	 */
+	private _db; 
 
-	constructor()
+	/**
+	 * The TaskService constructor 
+	 * @param {PouchDBService} private pouchdbService Service use to call pouchDB methods 
+	 */
+	constructor(private pouchdbService: PouchDBService)
 	{
-
+		this._db = this.pouchdbService.db; 
 	}
 
 	public getAll(): any 
 	{}
 
+	public validate(): any 
+	{}
+
+	/**
+	 * Get a task function 
+	 * @param  {string} id The id of a task 
+	 * @return {any}       Object with the docs 
+	 */
 	public get(id: string): any  
-	{}
+	{
+		return this._db.get(id).then((response) => 
+		{
+			return response;
+		}).catch((error) =>
+		{
+			throw error; 
+		});
+	}
 
+	/**
+	 * Add a task function 
+	 * @param  {Object} obj The task to add 
+	 * @return {any}        Object with the docs 
+	 */
 	public add(obj: Object): any 
-	{}
+	{
+		return this._db.put(obj).then((response) => 
+		{
+			return response;
+		}).catch((error) =>
+		{
+			throw error; 
+		});
+	}
 
-	public delete(id: string): any
-	{}
+	/**
+	 * Remove a task function 
+	 * @param  {string} doc The task to remove
+	 * @return {any}        Object with the docs 
+	 */
+	public remove(doc: string): any
+	{
+		return this._db.remove(doc).then((response) => 
+		{
+			return response;
+		}).catch((error) =>
+		{
+			throw error; 
+		});
+	}
 }
