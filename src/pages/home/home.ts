54b160d443d88b0834a2ff8aca73a1727b048725
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-12-05 16:44:07 +0100

    LLE - setup the task page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
-// import { GroupService } from '../../services/group.service';
-// import { UserService } from '../../services/user.service';
-// import { LogService } from '../../services/log.service';
 import { PouchDBService } from '../../services/pouchdb.service'
 
 // Import pages 
@@ -17,8 +14,7 @@ import { Notiflist } from '../notificationlist/notiflist';
   templateUrl: 'home.html'
 })
 export class HomePage {
-	// public groups;
-	// public users; 
+
   	constructor(public navCtrl: NavController, public navParams: NavParams, storage: Storage) 
   	{
 
@@ -26,9 +22,7 @@ export class HomePage {
 
 	ngOnInit()
 	{
-		// let userData = this.navParams.get('userParams').split(',');
-		// this.logService.setData(userData);
-		// this.pouchdbService.sync();
+
 	}
 
 	/**
