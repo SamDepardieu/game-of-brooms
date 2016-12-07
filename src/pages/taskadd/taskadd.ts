a1ac1519d31f9f22e6e7eec90d033b2d5d51da0a
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-12-07 09:43:18 +0100

    LLE - Add some comments

--- src/pages/taskadd/taskadd.ts
+++ src/pages/taskadd/taskadd.ts
@@ -14,23 +14,11 @@ export class Taskadd {
 	public taskName: string;  
 	public taskDescription: string; 
 	public taskPoints: number; 
-	public taskDeadline;
+	public taskDeadline: string;
 
 	constructor(private logService: LogService, private taskService: TaskService, public navCtrl: NavController) {
 
 	}
-
-
-	// private generateId(): string 
-	// {
-	// 	function s4()
-	// 	{
-	// 		return Math.floor((1 + Math.random()) * 0x10000)
-	// 		.toString(16)
-	// 		.substring(1);
-	// 	}
-	// 	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-	// }
 
 	public add():void 
 	{
