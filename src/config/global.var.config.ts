7deaba51f2fec260f096f72f05edce1f47ba9e43
Lucas Lepère <lucas.lepere@viacesi.fr>
2016-12-12 14:16:30 +0100

    LLE - The badger server method

--- src/config/global.var.config.ts
+++ src/config/global.var.config.ts
@@ -5,4 +5,7 @@
 export class GlobalConfig
 {
 	public static get API_URL(): string { return "http://brooms.b2plus.com/brooms"; }
+	// http://brooms.b2plus.com/brooms
+	// http://10.176.50.89:8000/brooms
 }
+
